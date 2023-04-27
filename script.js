1794ca2381b32e02b6dc5742a8a6d3a3b65546e9
Michael <michaelasare719@gmail.com>
2023-04-27 07:29:37 +0545

    Initial commit

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const container = document.querySelector('.container');
 const displayBox = document.querySelector('.display');
 const index = document.querySelector('#index');
-const allClearKey = document.getElementById('allClear');
-const clearKey = document.getElementById('clear');
 const equalSign = document.getElementById('equalSign');
 let inputText = document.querySelectorAll('.inputDisplay');
 let displayBoxElementsArray = displayBox.children;
 
 for(var i = 0; i < inputText.length; i++) {
 	inputText[i].addEventListener('click', (e) => {
-		if (inputText[i].innerHTML == "AC") {
-			displayBoxElementsArray = [];
-			console.log(displayBoxElementsArray)
+		if (e.target.innerText == "AC") {
+			const [index, ...operationElems] = displayBoxElementsArray;
+			allClear(operationElems);
+		} else if (e.target.innerText == "C") {
+			const [index, ...operationElems] = displayBoxElementsArray;
+			removeTextFromDisplay(operationElems[operationElems.length-1]);
+		} else {
+			appendTextToDisplay(e.target);
+			setColor(container);
 		}
-	appendTextToDisplay(e.target);
-	setColor(container);
-	})
+	});
 }
 
 equalSign.addEventListener('click', (displayBoxElementsArray) => {
@@ -38,13 +40,11 @@ equalSign.addEventListener('click', (displayBoxElementsArray) => {
 			power(operationElems);
 			break;
 		default:
-		//some code;
+		console.log(operationElems);
 		break;				
 		}
-
-		console.log(operationElems);
 	}
-});
+}); 
 
 window.onload = setInterval(() => indexToggle(index), 1000); 
 
@@ -64,6 +64,79 @@ function indexToggle(element) {
 		console.log('Output')
 	}
 	
+}
+
+// Addition function
+function add(array) {
+	var result;
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].innerText == "+") {
+			const rightOperators = getRightElem(array[i], array);
+			const leftOperators = getLeftElem(array[i], array);
+			result = (parseElems(leftOperators) + parseElems(rightOperators)).toString();
+		} else {
+			console.log(array)
+		}
+	}
+	console.log(result);
+	allClear(array);
+	appendTextToDisplay(result);
+}
+
+// Subtraction function
+function subtract(array) {
+	
+}
+
+// Multiplication function
+function multiply(array) {
+	
+}
+
+// Division function
+function divide(array) {
+	
+}
+
+// Power function
+function power(array) {
+	
+}
+
+// Function that gets element elements on the right-side of an operator.
+function getRightElem(element, array) {
+	const [element, ...rightOperators] = array;
+	return rightOperators;
+}
+
+// Function that gets elements on the left-side of an operator.
+function getLeftElem(elem, array) {
+	const [elem, ...leftOperators] = array.reverse();
+	return leftOperators;
+}
+
+
+// Function that accepts any array as a parameter and converts all the array item to the number data type.
+function parseElems(array) {
+	let parsedOperators = [];
+	for (var i = 0; i < array.length; i++) {
+		let parsedElem = parseInt(array[i]);
+		parsedOperators = [...parsedOperators, parsedElem];
+	}
+
+	return parsedOperators;
+}
+
+// Function that removes all innerText connected to the display element
+function removeTextFromDisplay(element) {
+	displayBox.removeChild(element);
+}
+
+// Function that removes all nodes appended to the display box
+function allClear(array) {
+	for (var i = 0; i < array.length; i++) {
+				removeTextFromDisplay(array[i]);
+			}
 }
 
 // Function that appends innerText to the display element
